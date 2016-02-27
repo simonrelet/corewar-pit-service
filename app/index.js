2ed594fa095e7f7b7f800da2031c95f1178265e0
Simon Relet <simon.relet@gmail.com>
2016-02-27 16:08:15 +0100

    Support malformed request error

--- app/index.js
+++ app/index.js
@@ -7,5 +7,5 @@ app.use('/', require('./pit-router.js'));
 var server = app.listen(4201, function() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Pit service listening at http://%s:%s', host, port);
+  console.log('Corewar pit service listening at http://%s:%s', host, port);
 });
--- app/pit-router.js
+++ app/pit-router.js
@@ -6,87 +6,94 @@ var fsp = require('fs-promise');
 var mkdirp = require('mkdirp');
 var exec = require('child_process').exec;
 
-var tmpFilesPath = 'tmpFilesPath/';
-var pitCmd = './bin/pit.py -b PACKED ';
+var shipsPath = 'bin/ships/';
+var pitCmd = 'java -jar bin/pit.jar ';
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-router.all('/', function(req, res, next) {
+router.all('/', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
 function ensureTmpDir() {
-  return new Promise(function(resolve, reject) {
-    mkdirp(tmpFilesPath, function(err) {
-      if(err) {
+  return new Promise(function (resolve, reject) {
+    mkdirp(shipsPath, function (err) {
+      if (err) {
         reject(err);
       } else {
         resolve();
       }
     });
-  }).catch(function(err) {
-    console.log('The directory \'' + tmpFilesPath + '\' could not be created: ' + err);
+  }).catch(function (err) {
+    console.log('The directory \'' + shipsPath + '\' could not be created: ' + err);
   });
 }
 
 function randomString(length) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+  return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 }
 
 function writeShipFile(content) {
-  var path = tmpFilesPath + randomString(32) + '.s';
-  return fsp.writeFile(path, content).then(function() {
+  var path = shipsPath + randomString(32) + '.s';
+  return fsp.writeFile(path, content).then(function () {
     return Promise.resolve(path);
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.log('Could not write file: \'' + path + '\': ' + err);
   });
 }
 
 function runPit(path) {
-  return new Promise(function(resolve, reject) {
-    exec(pitCmd + path, function(err, stdout, stderr) {
-      if(err) {
+  return new Promise(function (resolve, reject) {
+    exec(pitCmd + path, function (err, stdout, stderr) {
+      if (err) {
         reject(err);
       } else if (stderr) {
         reject(stderr);
       } else {
-        console.log('pit out:\n' + stdout);
+        console.log('Out: \'' + stdout + '\'');
         resolve({
           path: path,
-          res: {
-            name: 'Zork',
-            comment: 'This is the first ship ever.',
-            size: 34,
-            bin: 'f549ae767ab65c6e876f0'
-          }
+          res: JSON.parse(stdout)
         });
       }
     });
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.log('Error while runing pit: ' + err);
   });
 }
 
 function deleteShipFile(res) {
   return fsp.unlink(res.path)
-    .then(function() {
+    .then(function () {
       return Promise.resolve(res.res);
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.log('Could not delete file: \'' + res.path + '\': ' + err);
     });
 }
 
-router.post('/', function(req, res) {
-  ensureTmpDir().then(function() {
-    writeShipFile(req.body.src)
-      .then(runPit)
-      .then(deleteShipFile)
-      .then(function(r) {
-        res.json(r);
+router.post('/', function (req, res) {
+  ensureTmpDir().then(function () {
+    if (!req.body.src) {
+      console.log('The request doesn\'t contain a \'src\' attribute');
+      res.json({
+        errors: [{
+          line: 0,
+          msg: 'The request doesn\'t contain a \'src\' attribute'
+        }]
       });
+    } else {
+      writeShipFile(req.body.src)
+        .then(runPit)
+        .then(deleteShipFile)
+        .then(function (r) {
+          res.json(r);
+        });
+    }
   });
 });
 
